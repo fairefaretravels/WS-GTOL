cba79ef212d97225b65b603e89c222b7c53b7af0
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-18 22:12:11 -0400

    Update start_url to point to YouTube playlist

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
   "name": "Greater Tree Wayside",
   "short_name": "Wayside GTOL",
   "description": "Greater Tree Wayside Missionary Baptist Church",
-  "start_url": ".",
+  "start_url": "https://youtube.com/playlist?list=PLHR8R15a6EUuhqrJgvDMRPZhoXwfDLK_Q&si=qTPpijsjZ-4onD4d",
   "display": "standalone",
   "background_color": "#0c1a3a",
   "theme_color": "#0c1a3a",
